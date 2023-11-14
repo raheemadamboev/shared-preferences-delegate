1ec96cadc837f6c49334099cadfd4832882ac603
raheemadamboev <vividricemaryandmax@gmail.com>
2023-11-15 00:15:56 +0500

    Use delegate;

--- app/src/main/java/xyz/teamgravity/sharedpreferencesdelegate/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/sharedpreferencesdelegate/MainActivity.kt
@@ -1,6 +1,7 @@
 package xyz.teamgravity.sharedpreferencesdelegate
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,14 +11,21 @@ import androidx.compose.ui.Modifier
 import xyz.teamgravity.sharedpreferencesdelegate.ui.theme.SharedPreferencesDelegateTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var name by sharedPreferences("name")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("raheem", "onCreate: $name")
+        name = "pump it"
+        Log.d("raheem", "onCreate: $name")
         setContent {
             SharedPreferencesDelegateTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
                 }
             }
         }
